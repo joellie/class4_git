ea6848eb1c6b855c71d6524fac130ddcafbf324d
USER <USER@USER-PC>
2017-05-23 11:49:13 +0900

    branch로 수정

--- test_git/src/kr/co/sist/test/Test.java
+++ test_git/src/kr/co/sist/test/Test.java
@@ -5,6 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		int i=0;
 		System.out.println("변수 값 i="+i);
+		//jelly2가 수정
+		int j=0;
+		System.out.println("j="+j);
 	}//main
 
 }//class
